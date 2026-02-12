c83ecce07cf1578da8a5285a69b7aa6d288654d9
kerveros12v <kerveros12v@aol.com>
2026-02-11 22:38:51 -0500

    transacciones terminadas

--- JSNivel1Reto55/TrabajoGrupal2/transacciones.js
+++ JSNivel1Reto55/TrabajoGrupal2/transacciones.js
@@ -1,21 +1,21 @@
-cuentas=[
-    {numeroCuenta:"02234567", cedula:"1714616123",nombre:"Juan",apellido:"Perez",saldo:0.0},
-    {numeroCuenta:"02345211",cedula:"1281238233",nombre:"Felipe",apellido:"Caicedo",saldo:0.0}
+cuentas = [
+    { numeroCuenta: "02234567", cedula: "1714616123", nombre: "Juan", apellido: "Perez", saldo: 0.0 },
+    { numeroCuenta: "02345211", cedula: "1281238233", nombre: "Felipe", apellido: "Caicedo", saldo: 0.0 }
 ]
 
-cargar=function(){
+cargar = function () {
     mostrarComponente("divTransacciones");
     ocultarComponente("divCuentas");
     ocultarComponente("divMovimientos");
-    
+
 }
 
 /*
     Busca la cuenta en el arreglo en función del número de cuenta,
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
-buscarCuenta=function(numeroCuenta){
-  let cuentaEncontrada = null;
+buscarCuenta = function (numeroCuenta) {
+    let cuentaEncontrada = null;
     for (let i = 0; i < cuentas.length; i++) {
         if (cuentas[i].numeroCuenta == numeroCuenta) {
             cuentaEncontrada = cuentas[i];
@@ -25,37 +25,69 @@ buscarCuenta=function(numeroCuenta){
     return cuentaEncontrada;
 }
 
-ejecutarBusqueda=function(){
+ejecutarBusqueda = function () {
     //toma el numero de cuenta de la caja de texto
     //invoca a buscarCuenta y guarda el resultado en una variable
     //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
+    let campoCuenta = recuperarTexto("txtcuentaBuscar");
+    let cuentaLocalizada = buscarCuenta(campoCuenta);
+    if (cuentaLocalizada != null) {
+        alert("Cuenta: "+cuentaLocalizada.numeroCuenta + "---Nombre: " + cuentaLocalizada.nombre + " " + cuentaLocalizada.apellido + "---Saldo: " + cuentaLocalizada.saldo);
+    } else {
+        alert("No existe la cuenta");
+    }
 }
 
-depositar=function(numeroCuenta,monto){
+depositar = function (numeroCuenta, monto) {
     let cuentaAfectada;
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    cuentaAfectada = buscarCuenta(numeroCuenta);
     //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+    cuentaAfectada.saldo += monto;
 }
 
-ejecutarDeposito=function(){
+ejecutarDeposito = function () {
     //Toma el numero de cuenta ingresado en la caja de texto
+    let campoCuenta = recuperarTexto("txtcuentaBuscar");
+
     //Toma el monto ingresado en la caja de texto
+    let campoMonto = recuperarFloat("txtmonto");
+
     //invoca a depositar
+    depositar(campoCuenta, campoMonto);
     //Muestra un mensaje TRANSACCION EXITOSA
+   // alert("TRANSACCION EXITOSA");
     //Muestra en pantalla el nuevo saldo de la cuenta
+    alert("Saldo actual: " + buscarCuenta(campoCuenta).saldo);
+}
+ejecutarRetirar = function () {
+    //Toma el numero de cuenta ingresado en la caja de texto
+    let campoCuenta = recuperarTexto("txtcuentaBuscar");
+
+    //Toma el monto ingresado en la caja de texto
+    let campoMonto = recuperarFloat("txtmonto");
+
+    //invoca a retirar
+    retirar(campoCuenta, campoMonto);
+    //Muestra un mensaje TRANSACCION EXITOSA
+   // alert("TRANSACCION EXITOSA");
+    //Muestra en pantalla el nuevo saldo de la cuenta
+    alert("Saldo actual: " + buscarCuenta(campoCuenta).saldo);
 }
 
-depositar=function(numeroCuenta,monto){
+retirar = function (numeroCuenta, monto) {
     let cuentaAfectada;
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
-}
-
-retirar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    cuentaAfectada = buscarCuenta(numeroCuenta);
     //Valida si la cuenta tiene el saldo suficiente para retirar el monto
-    //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
-    //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
-    //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
+    if (cuentaAfectada.saldo > monto) {
+        //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
+        cuentaAfectada.saldo -= monto;
+         //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
+         alert("TRANSACCION EXITOSA");
+    } else {
+        //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
+        alert("SALDO INSUFICIENTE");
+    }
+   
 }
